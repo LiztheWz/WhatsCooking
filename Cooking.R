4c507309943cb8696691075600ecda25baeb8a35
Liz Mendoza <lizeth.mendoza0105@gmail.com>
2025-12-03 21:32:39 -0700

    hw 1 complete

---
+++ Cooking.R
@@ -0,0 +1,81 @@
+library(jsonlite)
+library(tidytext)
+library(tidyverse)
+library(stringr)
+library(dplyr)
+library(tidymodels)
+library(vroom)
+
+
+train_cooking <- read_file("train.json") %>% fromJSON()
+test_cooking <- read_file("test.json") %>% fromJSON()
+train_cooking <- train_cooking %>% unnest(ingredients)
+test_cooking <- test_cooking %>% unnest(ingredients)
+
+
+train_cooking <- train_cooking %>%
+  group_by(id, cuisine) %>% 
+  summarize(cuisine = first(cuisine),
+            num_ingredients = n(),
+            has_salt = as.integer(any(ingredients == "salt")),
+            has_milk = as.integer(any(ingredients == "milk")),
+            has_sugar = as.integer(any(ingredients == "sugar")),
+            # ingredients = list(ingredients),
+            .groups = "drop")
+
+test_cooking <- test_cooking %>%
+  group_by(id) %>% 
+  summarize(num_ingredients = n(),
+            has_salt = as.integer(any(ingredients == "salt")),
+            has_milk = as.integer(any(ingredients == "milk")),
+            has_sugar = as.integer(any(ingredients == "sugar")),
+            # ingredients = list(ingredients),
+            .groups = "drop")
+
+
+cooking_recipe <- recipe(cuisine ~ ., data = train_cooking) %>%
+  step_mutate(across(where(is.character), as.factor)) %>% 
+  step_dummy(all_nominal_predictors())
+
+
+rando_mod <- rand_forest(mtry = tune(),
+                              min_n = tune(),
+                              trees = 500) %>%
+  set_engine("ranger") %>% set_mode(mode = "classification")
+
+cooking_workflow <- workflow() %>%
+  add_recipe(cooking_recipe) %>%
+  add_model(rando_mod)
+
+rf_params <- parameters(mtry(),
+                        min_n()) %>% finalize(train_cooking)
+
+tuning_grid <- grid_regular(rf_params,
+                            levels = 5) # L^2 total tuning possibilities
+
+folds <- vfold_cv(train_cooking, v = 3, repeats = 1)
+
+CV_results <- cooking_workflow %>%
+  tune_grid(resamples = folds,
+            grid = tuning_grid,
+            metrics = metric_set(roc_auc))
+# roc_auc, f_meas, sens, recall, spec, precision, accuracy
+
+bestTune <- CV_results %>% select_best(metric = "roc_auc")
+
+final_wf <- cooking_workflow %>%
+  finalize_workflow(bestTune) %>%
+  fit(data = train_cooking)
+
+cooking_predictions <- final_wf %>%
+  predict(new_data = test_cooking, type = "class") %>%
+  bind_cols(test_cooking %>% select(id)) %>%
+  select(id, cuisine = .pred_class)
+
+vroom_write(cooking_predictions, "random_forest_predictions.csv", delim = ',')
+
+
+
+
+
+
